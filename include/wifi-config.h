e69f2131d8b46f1bf1b1fa1f9a6004741326dc57
Noah Beidelman <nbeidelman9@gmail.com>
2024-07-07 15:50:37 -0700

    adjust wifi config

---
+++ include/wifi-config.h
@@ -0,0 +1,2 @@
+#define WIFI_SSID "ASUS_10"
+#define WIFI_PASSWORD "Bei*cNLb$224"
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 #include <WiFi.h>
-#include <WiFiMulti.h>
-#include "wifi.h"
+#include <AsyncTCP.h>
+#include "wifi-config.h"
 
 Servo myServo;
-WiFiServer server(80);
+AsyncWebServer server(80);
 
 const uint8_t servoPin = 18;
 const uint8_t btn1Pin = 21;
 const uint8_t btn2Pin = 34;
 const uint8_t ledBuiltinPin = 2;
+const char* PARAM_MESSAGE = "message";
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
   // Attach the servo to the specified pin
@@ -20,40 +27,72 @@ void setup() {
   pinMode(btn1Pin, INPUT);
   pinMode(btn2Pin, INPUT);
   pinMode(ledBuiltinPin, OUTPUT);
-  digitalWrite(ledBuiltinPin, HIGH);
+  
   // Initialize serial communication for debugging
   Serial.begin(115200);
 
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  //digitalWrite(ledBuiltinPin, WiFi.status() != WL_CONNECTED);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.printf("WiFi Failed!\n");
+      return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(200, "text/plain", "Hello, world");
+  });
+
+  // Send a GET request to <IP>/get?message=<message>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      String message;
+      if (request->hasParam(PARAM_MESSAGE)) {
+          message = request->getParam(PARAM_MESSAGE)->value();
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message);
+  });
+
+  // Send a POST request to <IP>/post with a form field message set to <message>
+  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+      String message;
+      if (request->hasParam(PARAM_MESSAGE, true)) {
+          message = request->getParam(PARAM_MESSAGE, true)->value();
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + message);
+  });
+
+  server.onNotFound(notFound);
+
+  server.begin();
 }
 
 void loop() {
-  // Read button states
-  int btn1State = digitalRead(btn1Pin);
-  int btn2State = digitalRead(btn2Pin);
-  
-  // Control servo based on button states
-  if (btn1State == HIGH) {
-    Serial.println("Turning counterclockwise");
-    myServo.write(0); // Set servo to counterclockwise direction
-  } else if (btn2State == HIGH) { 
-    Serial.println("Turning clockwise");
-    myServo.write(180); // Set servo to clockwise direction
-  } else {
-    Serial.println("Stopping");
-    myServo.write(92); // Stop servo at midpoint
-  }
-  
-  delay(50); // Delay to debounce and stabilize readings
+
+
 }
+
+// void loop() {
+//   // Read button states
+//   int btn1State = digitalRead(btn1Pin);
+//   int btn2State = digitalRead(btn2Pin);
+  
+//   // Control servo based on button states
+//   if (btn1State == HIGH) {
+//     Serial.println("Turning counterclockwise");
+//     myServo.write(0); // Set servo to counterclockwise direction
+//   } else if (btn2State == HIGH) { 
+//     Serial.println("Turning clockwise");
+//     myServo.write(180); // Set servo to clockwise direction
+//   } else {
+//     //Serial.println("Stopping");
+//     myServo.write(92); // Stop servo at midpoint
+//   }
+  
+//   delay(50); // Delay to debounce and stabilize readings
+// }
