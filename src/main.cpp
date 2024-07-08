c7a1922120a67f20b1eb2ea4b4022e6f53130cd1
Noah Beidelman <nbeidelman9@gmail.com>
2024-07-08 00:12:21 -0700

    initial blocking code for open and close

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,16 @@ const uint8_t servoPin = 18;
 const uint8_t btn1Pin = 21;
 const uint8_t btn2Pin = 34;
 const uint8_t ledBuiltinPin = 2;
+
 const char* PARAM_DIRECTION = "direction";
 const char* PARAM_STOP = "stop";
+enum windowState {
+    WIN_CLOSED = 0,
+    WIN_OPEN,
+    WIN_TRANSITION_CLOSE,
+    WIN_TRANSITION_OPEN
+};
+windowState state = WIN_CLOSED;
 
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
@@ -65,6 +73,11 @@ void setup() {
   // Send a POST request to <IP>/rotate with a form field message set to direction
   server.on("/rotate", HTTP_POST, [](AsyncWebServerRequest *request){
       String direction;
+      if (state == WIN_TRANSITION_CLOSE || state == WIN_TRANSITION_OPEN) {
+        request->send(403, "text/plain", "Cannot control encoder" + state);
+        return;
+      }
+
       if (request->hasParam(PARAM_DIRECTION, true)) {
           direction = request->getParam(PARAM_DIRECTION, true)->value();
           if (direction == "clockwise") {
@@ -79,6 +92,37 @@ void setup() {
              Serial.println("stopping");
              myServo.write(92);
           }
+          else {
+            direction = "Invalid Direction";
+          }
+      } else {
+          direction = "Invalid Direction";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + direction);
+  });
+
+  // Send a POST request to <IP>/rotate with a form field message set to direction
+  server.on("/curtain", HTTP_POST, [](AsyncWebServerRequest *request){
+      String direction;
+      if (state == WIN_TRANSITION_CLOSE || state == WIN_TRANSITION_OPEN) {
+        request->send(403, "text/plain", "Cannot control encoder" + state);
+        Serial.print(state);
+        return;
+      }
+
+      if (request->hasParam(PARAM_DIRECTION, true)) {
+          direction = request->getParam(PARAM_DIRECTION, true)->value();
+          if (direction == "open") {
+            Serial.println("Opening Window");
+            state = WIN_TRANSITION_OPEN;
+          }
+          else if (direction == "close") {
+            Serial.println("Closing Window");
+            state = WIN_TRANSITION_CLOSE;
+          }
+          else {
+            direction = "Invalid Direction";
+          }
       } else {
           direction = "Invalid Direction";
       }
@@ -91,21 +135,36 @@ void setup() {
 }
 
 void loop() {
-  // Read button states
-  int btn1State = digitalRead(btn1Pin);
-  int btn2State = digitalRead(btn2Pin);
-  
-  // Control servo based on button states
-  if (btn1State == HIGH) {
-    Serial.println("Turning counterclockwise");
-    myServo.write(0); // Set servo to counterclockwise direction
-  } else if (btn2State == HIGH) { 
-    Serial.println("Turning clockwise");
-    myServo.write(180); // Set servo to clockwise direction
-  } else {
-    //Serial.println("Stopping");
-    //myServo.write(92); // Stop servo at midpoint
-  }
-  
-  delay(50); // Delay to debounce and stabilize readings
+    if (state == WIN_TRANSITION_CLOSE) {
+        myServo.write(0);
+        delay(1000);
+        myServo.write(92);
+        state = WIN_CLOSED;
+    }
+    else if (state == WIN_TRANSITION_OPEN) {
+        myServo.write(180);
+        delay(1000);
+        myServo.write(92);
+        state = WIN_OPEN;
+    }
 }
+
+// void loop() {
+//   // Read button states
+//   int btn1State = digitalRead(btn1Pin);
+//   int btn2State = digitalRead(btn2Pin);
+  
+//   // Control servo based on button states
+//   if (btn1State == HIGH) {
+//     Serial.println("Turning counterclockwise");
+//     myServo.write(0); // Set servo to counterclockwise direction
+//   } else if (btn2State == HIGH) { 
+//     Serial.println("Turning clockwise");
+//     myServo.write(180); // Set servo to clockwise direction
+//   } else {
+//     //Serial.println("Stopping");
+//     //myServo.write(92); // Stop servo at midpoint
+//   }
+  
+//   delay(50); // Delay to debounce and stabilize readings
+// }
